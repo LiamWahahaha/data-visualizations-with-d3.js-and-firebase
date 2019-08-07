97a1e2fc5509982a39563eb1cbc206a840ce4caa
Liam <liliwang@cs.stonybrook.edu>
2019-08-07 10:54:49 -0400

    Use data with D3

---
+++ lesson_3/index.js
@@ -0,0 +1,28 @@
+const data = [
+    {width: 200, height: 100, fill: 'purple'},
+    {width: 100, height: 60, fill: 'pink'},
+    {width: 50, height: 30, fill: 'red'},
+]
+const svg = d3.select('svg');
+
+// Join data to rects
+const rects = svg.selectAll('rect')
+    .data(data);
+
+// Add attrs to rects already in the DOM
+rects.attr('width', (d, i, n) => { // arrow function
+    console.log(this)
+    console.log(i)
+    console.log(n)
+    console.log(n[i])
+    return d.width })
+    .attr('height', d => d.height) // in some conditions, we can get rid of the curly brackets
+    .attr('fill', function(d) { return d.fill });
+
+// Append the enter selection to DOM
+rects.enter()
+    .append('rect')
+    .attr('width', (d, i, n) => d.width)
+    .attr('height', d => d.height)
+    .attr('fill', d => d.fill);
+console.log(rects);
